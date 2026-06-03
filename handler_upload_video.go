8763af274b8cfdae1f151b7d2c8464be48345786
Lucas Schweickert <cyberfly@gmx.net>
2026-06-03 18:32:17 +0200

    added video upload to AWS S3 feature

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,7 +1,117 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+	"log"
+	"mime"
 	"net/http"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {}
+func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request) {
+	maxUploadSize := int64(1 << 30) // 1GB
+	r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
+
+	videoIDString := r.PathValue("videoID")
+	videoID, err := uuid.Parse(videoIDString)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid ID", err)
+		return
+	}
+
+	token, err := auth.GetBearerToken(r.Header)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't find JWT", err)
+		return
+	}
+
+	userID, err := auth.ValidateJWT(token, cfg.jwtSecret)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Couldn't validate JWT", err)
+		return
+	}
+
+	log.Println("uploading video", videoID, "by user", userID)
+
+	videoMetaData, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video data", err)
+		return
+	}
+
+	if videoMetaData.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User doesn't have permission to upload this video", nil)
+		return
+	}
+
+	multipartFile, header, err := r.FormFile("video")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't read video file", err)
+		return
+	}
+	defer multipartFile.Close()
+
+	mediaType, _, err := mime.ParseMediaType(header.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse media type", err)
+		return
+	}
+	if mediaType != "video/mp4" {
+		respondWithError(w, http.StatusBadRequest, "Unsupported media type. Only JPEG and PNG are allowed.", nil)
+		log.Println("Unsupported media type of the thumbnail:", mediaType)
+		return
+	}
+	fileExtension := strings.Split(mediaType, "/")[1]
+
+	tempFile, err := os.CreateTemp("", "tubely-upload."+fileExtension)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create temp file", err)
+		return
+	}
+	defer tempFile.Close()
+	defer os.Remove(tempFile.Name())
+
+	_, err = io.Copy(tempFile, multipartFile)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't copy video file", err)
+		return
+	}
+	tempFile.Seek(0, io.SeekStart)
+
+	randomNum := make([]byte, 32)
+	rand.Read(randomNum)
+	randomString := hex.EncodeToString(randomNum)
+
+	s3ObjectInput := &s3.PutObjectInput{
+		Bucket:      aws.String(cfg.s3Bucket),
+		Key:         aws.String(randomString + "." + fileExtension),
+		Body:        tempFile,
+		ContentType: aws.String(mediaType),
+	}
+
+	_, err = cfg.s3Client.PutObject(r.Context(), s3ObjectInput)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't upload video file", err)
+		return
+	}
+
+	videoURL := "https://" + cfg.s3Bucket + ".s3." + cfg.s3Region + ".amazonaws.com/" + randomString + "." + fileExtension
+
+	videoMetaData.VideoURL = &videoURL
+
+	err = cfg.db.UpdateVideo(videoMetaData)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video URL in database", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoMetaData)
+}
